43ea5cbc9c4a9e86e2f673b7b84113ed62a1d8da
Harshad Khandare <harshadkhandare9000@gmail.com>
2025-06-22 16:33:53 +0530

    Fixed CORS

--- mychat/src/__init__.py
+++ mychat/src/__init__.py
@@ -34,7 +34,7 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://mychat-now.netlify.app/",
+    "https://mychat-now.netlify.app",
 ]
 
 app.add_middleware(
